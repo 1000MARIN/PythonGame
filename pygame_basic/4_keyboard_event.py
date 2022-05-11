1f9dfde8062ce8175acb0b1971acb4624b66ae82
1000marin <leexjin@gmail.com>
2022-05-11 11:34:10 +0900

    PythonGame

--- pygame_basic/4_keyboard_event.py
+++ pygame_basic/4_keyboard_event.py
@@ -34,16 +34,34 @@ while running:
 
         if event.type == pygame.KEYDOWN:    # 키가 눌러졌는지 확인
             if event.key == pygame.K_LEFT:  # 캐릭터를 왼쪽으로
-                to_x -= 5                 # to_x = to_x - 5
-            if event.key == pygame.K_RIGHT:  # 캐릭터를 오른쪽으로
-                pass
-            if event.key == pygame.K_UP:  # 캐릭터를 위로
-                pass
-            if event.key == pygame.K_DOWN:  # 캐릭터를 아래로
-                pass 
+                to_x -= 1                 # to_x = to_x - 5
+            elif event.key == pygame.K_RIGHT:  # 캐릭터를 오른쪽으로
+                to_x += 1
+            elif event.key == pygame.K_UP:  # 캐릭터를 위로 
+                to_y -= 1
+            elif event.key == pygame.K_DOWN:  # 캐릭터를 아래로
+                to_y += 1 
 
+        if event.type == pygame.KEYUP: # 방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
 
+    character_x_pos += to_x
+    character_y_pos += to_y
 
+    # 가로 경계값 처리
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 세로 경계 값 처리
+    if character_y_pos < 0: 
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height 
     screen.blit(background, (0, 0))     # 배경 그리기
     screen.blit(character, (character_x_pos, character_y_pos))   # 캐릭터 그리기
 
